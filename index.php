5575eafe5fb323486551b416a6bbf8d9169fe66a
GBD18623 <66302927+GBD18623@users.noreply.github.com>
2020-06-26 15:57:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,13 +14,10 @@ if (empty(getenv("DATABASE_URL")))
  $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb','postgres','123456'); 
 }else{ 
  $db = parse_url(getenv("DATABASE_URL")); 
- $pdo = new PDO("pgsql:" . sprintf( 
-
- 
- 
- 
-  "ec2-54-86-170-8.compute-1.amazonaws.com;port=5432;user=fgfjvddasygorg; 
-  password=054f935ebb5384f6eaa169ef96ff3d0768cfeb5918139bd588d885556a997c02;dbname=d935q1p1jmdqtl", 
+ $pdo = new PDO("pgsql:" . sprintf("ec2-54-86-170-8.compute-1.amazonaws.com;
+ port=5432;
+ user=fgfjvddasygorg;password=054f935ebb5384f6eaa169ef96ff3d0768cfeb5918139bd588d885556a997c02;
+ dbname=d935q1p1jmdqtl", 
      $db["host"], 
      $db["port"], 
      $db["user"], 
@@ -30,7 +27,7 @@ if (empty(getenv("DATABASE_URL")))
 if($pdo ===false){ 
  echo "ERROR:Could not connect Database"; 
 } 
-$sql="INSERT INTO toy(toyid, toyname)" 
+$sql="INSERT INTO Toys(toyid, toyname)" 
      . " VALUES('$_POST[toyid]','$_POST[toyname]')"; 
 $stmt = $pdo-> prepare($sql); 
 if (is_null($_POST[toyid])){ 
